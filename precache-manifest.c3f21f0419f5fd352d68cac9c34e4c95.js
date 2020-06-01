af3b6b9b1c7b919d7770122084685ed280cf3cf8
Angelique Husson <44775743+AngeliqueHusson@users.noreply.github.com>
2020-06-01 19:33:48 +1000

    Updates

--- precache-manifest.c3f21f0419f5fd352d68cac9c34e4c95.js
+++ precache-manifest.c3f21f0419f5fd352d68cac9c34e4c95.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55afc467f9ceea766c361445c96423fe",
+    "revision": "78467a4b7b3f8a9beb0d4e41f42e96da",
     "url": "/MathClassification/index.html"
   },
   {
-    "revision": "c3d9b21dd9859bb0389c",
+    "revision": "d7933a5ae7794a494a6f",
     "url": "/MathClassification/static/css/main.5130dcd6.chunk.css"
   },
   {
-    "revision": "b07156e37356188448b8",
-    "url": "/MathClassification/static/js/2.1119b74a.chunk.js"
+    "revision": "6135ece94b126bc5757c",
+    "url": "/MathClassification/static/js/2.0629c811.chunk.js"
   },
   {
     "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
-    "url": "/MathClassification/static/js/2.1119b74a.chunk.js.LICENSE.txt"
+    "url": "/MathClassification/static/js/2.0629c811.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c3d9b21dd9859bb0389c",
-    "url": "/MathClassification/static/js/main.8d81038c.chunk.js"
+    "revision": "d7933a5ae7794a494a6f",
+    "url": "/MathClassification/static/js/main.477630d0.chunk.js"
   },
   {
     "revision": "c34d7b2a3fc0e9477c4b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MathClassification/precache-manifest.28a084b1260ad768a450170ed889eac2.js"
+  "/MathClassification/precache-manifest.c3f21f0419f5fd352d68cac9c34e4c95.js"
 );
 
 self.addEventListener('message', (event) => {
